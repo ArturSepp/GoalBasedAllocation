02a402d8bd44d48a8e5175678967ed32b754a62b
ArturSepp <artursepp@gmail.com>
2026-08-20 06:35:05 +0200

    Point documentation metadata to Read the Docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,6 @@
 """Sphinx configuration for GoalBasedAllocation."""
 
+import os
 from importlib.metadata import version as package_version
 
 project = "GoalBasedAllocation"
@@ -20,7 +21,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinxdoc"
 html_title = "GoalBasedAllocation documentation"
 html_short_title = "GoalBasedAllocation"
-html_baseurl = "https://artursepp.github.io/GoalBasedAllocation/"
+html_baseurl = os.environ.get(
+    "READTHEDOCS_CANONICAL_URL",
+    "https://goalbasedallocation.readthedocs.io/en/latest/",
+)
 html_extra_path = ["robots.txt", "googleccb1e876a2b4bf72.html"]
 
 sitemap_url_scheme = "{link}"
